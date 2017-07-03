df06f2f7c7d5546d9834232191cbf2867a32f946
Simon <simon980425@126.com>
2017-07-03 16:39:59 -0700

    stage

--- Crawler/StockDB/settings.py
+++ Crawler/StockDB/settings.py
@@ -46,9 +46,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'StockDB.middlewares.StockdbSpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+   'Middlewares_agent': 200,
+}
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
