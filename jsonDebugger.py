2d77d17ba26c44aeef433a004f78af3cef266168
Simon <simon980425@126.com>
2018-03-20 23:46:07 -0700

    ädd quote summary store json

--- jsonDebugger.py
+++ jsonDebugger.py
@@ -10,4 +10,5 @@ response = requests.get("https://finance.yahoo.com/quote/AAPL")
 soup = BeautifulSoup(response.text, "lxml")
 script = soup.find("script",text=re.compile("root.App.main")).text
 data = loads(re.search("root.App.main\s+=\s+(\{.*\})", script).group(1))
-print(json.dumps(data, sort_keys=False, indent=4))
+stores = data["context"]["dispatcher"]["stores"]['QuoteSummaryStore']
+print(json.dumps(stores, sort_keys=True, indent=4))
