f6bb9829a820798389bbda44868c6ca3fb2b4e32
Simon <simon980425@126.com>
2018-03-22 14:19:14 -0700

    test

--- crawler_stock_profile/crawler/settings.py
+++ crawler_stock_profile/crawler/settings.py
@@ -23,12 +23,12 @@ ROBOTSTXT_OBEY = False
 USER_AGENT ='Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:41.0) Gecko/20100101 Firefox/41.0'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 4
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 5
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
